ab757f8bea3261eb443e3167c5c103b1d01ad10b
Ewa Lis <ewa.natalia.lis@gmail.com>
2021-01-18 13:17:37 +0100

    rules fragment

--- app/src/main/java/com/example/projektgrupowy/view/RulesFragment.kt
+++ app/src/main/java/com/example/projektgrupowy/view/RulesFragment.kt
@@ -5,12 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.example.projektgrupowy.R
-import com.example.projektgrupowy.model.PlayerWithoutId
-import com.example.projektgrupowy.viewmodel.PlayerViewModel
+import kotlinx.android.synthetic.main.fragment_local_player.*
+import kotlinx.android.synthetic.main.fragment_rules.*
 import kotlinx.android.synthetic.main.fragment_welcome.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -20,14 +18,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [WelcomeFragment.newInstance] factory method to
+ * Use the [RulesFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class WelcomeFragment : Fragment() {
+class RulesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var  playerViewModel:PlayerViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,29 +38,18 @@ class WelcomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        playerViewModel= ViewModelProvider(requireActivity()).get(PlayerViewModel::class.java)
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        return inflater.inflate(R.layout.fragment_rules, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        buttonGoToLocalPlayer.setOnClickListener { view -> view.findNavController().navigate(R.id.action_welcomeFragment_to_localPlayerFragment) }
-        LetsPlayButton.setOnClickListener { view ->
-            run {
-                if (nickEditText.text.toString() != "") {
-                    playerViewModel.addPlayer(nickEditText.text.toString())
-                    view.findNavController().navigate(R.id.action_welcomeFragment_to_gameFragment)
-                } else {
-                    Toast.makeText(context, "Wpisz nick", Toast.LENGTH_SHORT).show()
-                }
+        buttonBackFromRules.setOnClickListener { view -> view.findNavController().navigate(R.id.action_rulesFragment_to_welcomeFragment) }
 
-            }
 
-        }
-        RulesButton.setOnClickListener { view -> view.findNavController().navigate(R.id.action_welcomeFragment_to_rulesFragment) }
     }
-
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -71,12 +57,12 @@ class WelcomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment WelcomeFragment.
+         * @return A new instance of fragment RulesFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            WelcomeFragment().apply {
+            RulesFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
